f391b10b32342ea51f8f649adaac09a7a009a259
syedmyali <syounusa@yahoo.com>
2024-07-21 23:50:34 +0100

    changes made

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,10 +4,10 @@ import { FiMail } from "react-icons/fi";
 import { MdOutlineBadge } from "react-icons/md";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
-import { IoDocumentTextOutline } from "react-icons/io5";
+// import { IoDocumentTextOutline } from "react-icons/io5";
 
 import profile_pic from "../assets/profile1.png";
-import resume from "../assets/CV_Syed.pdf";
+// import resume from "../assets/CV_Syed.pdf";
 
 const Hero = () => {
   const {
@@ -36,9 +36,9 @@ const Hero = () => {
         <a href="#contact">
           <button className="primary-btn"> Contact Me</button>
         </a>
-        <a href={resume} target="_blank">
+        {/* <a href={resume} target="_blank">
           <button className="primary-btn ml-3"> Resume </button>
-        </a>
+        </a> */}
       </div>
 
       <div className="flex gap-2 justify-center md:gap-3 lg:gap-5 z-[1]">
@@ -93,11 +93,11 @@ const Hero = () => {
                 <FaXTwitter className="text-lg md:text-xl" />
               </a>
             </div>
-            <div className="bg-blue-800/30 p-2 rounded">
+            {/* <div className="bg-blue-800/30 p-2 rounded">
               <a href={resume} target="_blank">
                 <IoDocumentTextOutline className="text-lg md:text-xl" />
               </a>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
